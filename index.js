9c79f9091c982cb8a5f3287008c02c5485c75449
Daniil <filippovdaniil@gmail.com>
2017-12-14 19:04:17 +0700

    (f) Fixed index.js ESLint errors

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-var sassdoc = require('sassdoc');
-var express = require('express');
-var sassDocCommand = require('./lib/commands/sassdoc');
+const express = require('express');
+const sassDocCommand = require('./lib/commands/sassdoc');
 
 module.exports = {
   name: 'ember-cli-sassdoc',
 
-  config: function (environment, appConfig) {
-    this.docDir = appConfig.sassDocDir || 'sassdoc'
+  config(environment, appConfig) {
+    this.docDir = appConfig.sassDocDir || 'sassdoc';
     this.docRoute = appConfig.sassDocRoute || 'sassdoc';
     return {
       sassDocDir: this.docDir,
       sassDocRoute: this.docRoute,
-    }
+    };
   },
 
-  serverMiddleware: function (config) {
-    var app = config.app;
-    var options = config.options;
+  serverMiddleware(config) {
+    const app = config.app;
+    const options = config.options;
     app.use(options.baseURL + this.docRoute, express.static(this.docDir));
   },
 
-  includedCommands: function () {
+  includedCommands() {
     return {
-      'sassdoc': sassDocCommand,
+      sassdoc: sassDocCommand,
     };
   },
 };
